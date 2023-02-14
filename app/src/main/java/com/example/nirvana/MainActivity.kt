f54d082bdb6512466d306220ff8ef5913294aab7
intel <adityakhaladkar98@gmal.com>
2023-02-14 11:31:11 +0530

    set up nav graph and view binding

--- app/src/main/java/com/example/nirvana/MainActivity.kt
+++ app/src/main/java/com/example/nirvana/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.nirvana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nirvana.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
