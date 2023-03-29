47ecb704dcee2ce98a6ad78df7c53b9fa53abe12
intel <adityakhaladkar98@gmal.com>
2023-03-29 09:12:00 +0530

    bug fix 2, removed side nav and added static lottie animation

--- app/src/main/java/com/example/nirvana/view/ui/DashboardFrag.kt
+++ app/src/main/java/com/example/nirvana/view/ui/DashboardFrag.kt
@@ -1,27 +1,21 @@
 package com.example.nirvana.view.ui
 
-import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nirvana.R
-import com.example.nirvana.adapter.CategoryAdapter
 import com.example.nirvana.databinding.FragmentDashboardBinding
-import com.example.nirvana.model.CategoryModel
 import com.example.nirvana.view.fragment.AddSessionScreen
-import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class DashboardFrag : Fragment() {
     lateinit var binding: FragmentDashboardBinding
-    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     private fun authSignOut() {
         FirebaseAuth.getInstance().signOut()
@@ -35,30 +29,11 @@ class DashboardFrag : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentDashboardBinding.inflate(inflater, container, false)
 
-        // drawer layout instance to toggle the menu icon to open
-        // drawer and back button to close drawer
-        actionBarDrawerToggle = ActionBarDrawerToggle(
-            context as Activity?,
-            binding.dashboardDrawerLayout,
-            R.string.nav_open,
-            R.string.nav_close
-        )
-
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
-        binding.dashboardDrawerLayout.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.syncState()
-
-        (activity as AppCompatActivity?)!!.setSupportActionBar(binding.sideNavToolbar)
-
-
-        // to make the Navigation drawer icon always appear on the action bar
-        (activity as AppCompatActivity?)!!.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        (activity as AppCompatActivity?)!!.supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_menu_24)
-
         // Dashboard Features
-        category(binding = binding)
+        binding.lottieViewSports.setAnimation(R.raw.sports)
+        binding.lottieViewMusic.setAnimation(R.raw.music)
+        binding.lottieViewPetting.setAnimation(R.raw.pet)
+        binding.lottieViewPhoto.setAnimation(R.raw.photo)
 
         // Bottom Nav -> Onclick and Navigation
         binding.bottomNavigationView.setOnItemSelectedListener {
@@ -75,29 +50,7 @@ class DashboardFrag : Fragment() {
             }
         }
 
-        // Side nav bar
-        binding.nvView.setNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navSignOut -> authSignOut()
-            }
-            true
-        }
-
         return binding.root
-    }
-
-    private fun category(binding: FragmentDashboardBinding) {
-        binding.categoryRecyclerView.layoutManager =
-            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        binding.categoryRecyclerView.setHasFixedSize(true)
-        val data = listOf(
-            CategoryModel(R.raw.sports, "sports"),
-            CategoryModel(R.raw.music, "music"),
-            CategoryModel(R.raw.pet, "petting"),
-            CategoryModel(R.raw.photo, "photography")
-        )
-        val adapter = CategoryAdapter(data)
-        binding.categoryRecyclerView.adapter = adapter
     }
 
     private fun loadFragment(fragment: Fragment) {
@@ -105,11 +58,5 @@ class DashboardFrag : Fragment() {
             (activity as AppCompatActivity?)!!.supportFragmentManager.beginTransaction()
         transaction.add(R.id.containerLayout, fragment)
         transaction.commit()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            true
-        } else super.onOptionsItemSelected(item)
     }
 }
