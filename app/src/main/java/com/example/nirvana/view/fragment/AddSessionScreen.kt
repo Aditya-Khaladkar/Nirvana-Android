f72264de8ac4a31eaec8c7cf4a39818917f3c6ca
intel <adityakhaladkar98@gmal.com>
2023-04-03 09:26:53 +0530

    added viewmodel for add session frag

--- app/src/main/java/com/example/nirvana/view/fragment/AddSessionScreen.kt
+++ app/src/main/java/com/example/nirvana/view/fragment/AddSessionScreen.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.nirvana.R
 import com.example.nirvana.databinding.FragmentAddSessionScreenBinding
+import com.example.nirvana.viewmodel.AddSessionScreenViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 
 class AddSessionScreen : Fragment() {
    lateinit var binding: FragmentAddSessionScreenBinding
+   lateinit var mAuth: FirebaseAuth
+
+   // init viewmodel
+   lateinit var viewModel: AddSessionScreenViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,7 +26,13 @@ class AddSessionScreen : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentAddSessionScreenBinding.inflate(inflater, container, false)
 
+        // init firebase auth
+        mAuth = FirebaseAuth.getInstance()
 
+        // get email from firebase auth panel
+        binding.edEventEmail.setText(mAuth.currentUser?.email)
+//        viewModel = ViewModelProvider(this)[AddSessionScreenViewModel::class.java]
+//        viewModel.getEmailFromFirebase(binding, mAuth)
 
         return binding.root
     }
